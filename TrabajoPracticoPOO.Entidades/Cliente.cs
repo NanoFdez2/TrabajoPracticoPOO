f596daf4bd0bd199c40254e29097000cc6992f89
Nano <martinianofernandez.mf@gmail.com>
2025-06-23 02:24:50 -0300

    Se agregaron las entidades!

---
+++ TrabajoPracticoPOO.Entidades/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TrabajoPracticoPOO.Entidades
+{
+    public abstract class Cliente : IValidatableObject
+    {
+
+
+        public string nombre { get; set; }
+        public string DNI { get; set; }
+        public DateTime fechaAlta { get; set; }
+        public decimal costoMembresia { get; set; }
+        public int antiguedad { get; set; }
+        public int cantidadDias { get; set; }
+        public Localidad localidad { get; set; }
+        public Servicio servicio { get; set; }
+
+        public abstract decimal CalcularGastoMensual();
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+                yield return new ValidationResult("El nombre no puede ser nulo ni vacio!!!");
+
+            if (!Regex.IsMatch(DNI ?? "", @"^\d{8}$")) ;
+            yield return new ValidationResult("El DNI debe ser de 8 digitos!");
+
+            if (fechaAlta > DateTime.Now)
+                yield return new ValidationResult("La fecha de alta no puede ser mayor a la fecha actual");
+        }
+    }
+}
---
+++ TrabajoPracticoPOO.Entidades/SocioPremium.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPracticoPOO.Entidades
+{
+    public class SocioPremium : Cliente
+    {
+
+        public SocioPremium()
+        {
+            cantidadDias = 5;
+            costoMembresia = 35000 + 10000;
+            servicio = Servicio.GimnasioTrainer;
+        }
+        public override decimal CalcularGastoMensual()
+        {
+            return costoMembresia;
+        }
+    }
+}
