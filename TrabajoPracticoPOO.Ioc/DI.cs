4a6af389013aa04c92e75b4654a73de14589f95b
Fakusu <facuhgs@gmail.com>
2025-07-16 23:42:23 -0300

    Creacion de Clase DI

---
+++ TrabajoPracticoPOO.Ioc/DI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TrabajoPracticoPOO.Ioc
+{
+    public static class DI
+    {
+        public static IServiceProvider Configurar()
+        {
+            var servicios = new ServiceCollection();
+
+            servicios.AddScoped<IrepositorioClientes, RepositorioClientesLinq>();
+            servicios.AddScoped<IservicioClientes, ServicioClientes>();
+
+            return servicios.BuildServiceProvider();
+        }
+    }
+}
