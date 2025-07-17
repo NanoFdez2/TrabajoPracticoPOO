6b397e1137d5d10e41c9da4912dc9718d136c1bb
Nano <martinianofernandez.mf@gmail.com>
2025-07-17 00:47:10 -0300

    Se agregan las funcionalidades del data transfer object.

---
+++ TrabajoPracticoPOO.Servicios/dto/ValidationResultDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPracticoPOO.Servicios.dto
+{
+    public class ValidationResultDto
+    {
+        public bool EsValido { get; set; }
+        public List<string> Errores { get; set; } = new List<string>();
+    }
+}
